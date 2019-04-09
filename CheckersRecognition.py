bce193cc4ab08f5e1ce344d585d45e9d6b8bd2d2
PBiskup <p.biskup@expansio.pl>
2019-04-09 23:20:15 +0200

    perspective transform plus checkers detection

--- CheckersRecognition.py
+++ CheckersRecognition.py
@@ -45,7 +45,7 @@ lower_blue = np.array([100, 70, 70])  # 100,50,50
 upper_blue = np.array([130, 255, 255])
 
 #wczytanie zdjecia
-img = cv2.imread('zdj/b4.jpeg')
+img = cv2.imread('zdj/b10.jpeg')
 
 #zmniejszenie obrazu
 res = cv2.resize(img, None, fx=0.18, fy=0.18, interpolation=cv2.INTER_CUBIC)
@@ -81,6 +81,8 @@ for i in contours:
     img2 = cv2.line(img2, (cx, cy), (cx, cy), (128, 255, 187), 5)
     list_of_points.append([cx, cy])
 
+cv2.imshow('obrazek', img2)
+cv2.waitKey(0)
 
 list_of_points.reverse()
 print('1. ' + str(list_of_points))
@@ -102,35 +104,23 @@ dst = cv2.warpPerspective(res, M, (600, 600))
 
 cv2.imshow("obrazek", dst)
 cv2.waitKey()
+
+dst = cv2.medianBlur(dst, 7)
+gray_img = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+
+circles = cv2.HoughCircles(gray_img, cv2.HOUGH_GRADIENT, 1, 20, param1=35, param2=15, minRadius=30, maxRadius=35)  # 20, 25
+if len(circles):
+    circles = np.uint16(np.around(circles))
+
+index = 1
+for i in circles[0, :]:
+    print(str(index) + ': ' + str(i))
+    index += 1
+    # draw the outer circle
+    cv2.circle(dst, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    # draw the center of the circle
+    cv2.circle(dst, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+cv2.imshow('obrazek', dst)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
-
-#img = cv2.imread('zdj/7.jpg',0)
-#
-# res = cv2.resize(img,None,fx=0.18, fy=0.18, interpolation = cv2.INTER_CUBIC)
-# res = cv2.medianBlur(res,5)
-# cimg = cv2.cvtColor(res,cv2.COLOR_GRAY2BGR)
-#
-# #circles = cv2.HoughCircles(res,cv2.HOUGH_GRADIENT,1,100,param1=35,param2=15,minRadius=0,maxRadius=0) #20, 25
-# #circles = cv2.HoughCircles(res,cv2.HOUGH_GRADIENT,1,20,param1=35,param2=15,minRadius=20,maxRadius=25) #20, 25
-#
-# circles = cv2.HoughCircles(res,cv2.HOUGH_GRADIENT,1,20,param1=35,param2=15,minRadius=20,maxRadius=25) #20, 25
-#
-# circles = np.uint16(np.around(circles))
-#
-# index = 1
-#
-# for i in circles[0,:]:
-#     print(str(index) + ': ' + str(i))
-#     index += 1
-#     # draw the outer circle
-#     cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-#     # draw the center of the circle
-#     cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-#
-# cv2.imshow('detected circles',cimg)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
