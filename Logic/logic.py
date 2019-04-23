9bc85afacf3e2a106f184c15dfd817c49cc59c3f
MichalMaciaszek <39939699+MichalMaciaszek@users.noreply.github.com>
2019-04-23 22:59:32 +0200

    Single hit looking dumb, work dope

--- Logic/logic.py
+++ Logic/logic.py
@@ -102,43 +102,35 @@ def mozliwe_bicia(chessboard, wewnetrzne, bigVector):
                 wewnetrzne.append(chessboard.gameTiles[z].pieceOnTile.toString())
             zaszly_zmiany = 0
 
-        if chessboard.gameTiles[x].pieceOnTile.toString() == 'M':
-            if x == 7 or x == 23 or x == 39 or x == 14 or x == 30 or x == 46:
-                if chessboard.gameTiles[x + 7].pieceOnTile.toString() == 'm':
-                    if chessboard.gameTiles[x + 14].pieceOnTile.toString() == '-':
+        if wewnetrzne[x] == 'M' and (x == 7 or x == 23 or x == 39 or x == 14 or x == 30 or x == 46) and wewnetrzne[x + 7] == 'm' and wewnetrzne[x + 14] == '-':
                         wewnetrzne[x] = '-'
                         wewnetrzne[x + 7] = '-'
                         wewnetrzne[x+14] = 'M'
                         zaszly_zmiany = 1
                         bigVector.append(wewnetrzne.copy())
                         del wewnetrzne[:]
-            elif x == 8 or x == 24 or x == 40 or x==1 or x==17 or x==33:
-                if chessboard.gameTiles[x + 9].pieceOnTile.toString() == 'm':
-                    if chessboard.gameTiles[x + 18].pieceOnTile.toString() == '-':
+        elif wewnetrzne[x] == 'M' and (x == 8 or x == 24 or x == 40 or x==1 or x==17 or x==33) and wewnetrzne[x + 9] == 'm' and wewnetrzne[x + 18] == '-':
                         wewnetrzne[x] = '-'
                         wewnetrzne[x + 9] = '-'
                         wewnetrzne[x+18]='M'
                         bigVector.append(wewnetrzne.copy())
                         del wewnetrzne[:]
                         zaszly_zmiany = 1
-            elif x == 56 or x == 58 or x == 60 or x == 62 or x==49 or x==51 or x==53 or x ==55:
-                print()
-
-            else:
-                if chessboard.gameTiles[x + 9].pieceOnTile.toString() == 'm':
-                    if chessboard.gameTiles[x + 18].pieceOnTile.toString() == '-':
-                        wewnetrzne[x] = '-'
-                        wewnetrzne[x + 9] = '-'
-                        wewnetrzne[x + 18] = 'M'
-                        bigVector.append(wewnetrzne.copy())
-                        del wewnetrzne[:]
-                        for z in range(64):
-                            wewnetrzne.append(chessboard.gameTiles[z].pieceOnTile.toString())
-                if chessboard.gameTiles[x + 7].pieceOnTile.toString() == 'm':
-                    if chessboard.gameTiles[x + 14].pieceOnTile.toString() == '-':
-                        wewnetrzne[x] = '-'
-                        wewnetrzne[x + 7] = '-'
-                        wewnetrzne[x + 14] = 'M'
-                        bigVector.append(wewnetrzne.copy())
-                        del wewnetrzne[:]
-                        zaszly_zmiany = 1
+        elif wewnetrzne[x] == 'M' and (x == 56 or x == 58 or x == 60 or x == 62 or x==49 or x==51 or x==53 or x ==55):
+            print()
+        elif wewnetrzne[x] == 'M':
+            if wewnetrzne[x + 9] == 'm' and wewnetrzne[x + 18] == '-':
+                            wewnetrzne[x] = '-'
+                            wewnetrzne[x + 9] = '-'
+                            wewnetrzne[x + 18] = 'M'
+                            bigVector.append(wewnetrzne.copy())
+                            del wewnetrzne[:]
+                            for z in range(64):
+                                wewnetrzne.append(chessboard.gameTiles[z].pieceOnTile.toString())
+            if wewnetrzne[x] == 'M' and wewnetrzne[x + 7] == 'm' and wewnetrzne[x + 14] == '-':
+                            wewnetrzne[x] = '-'
+                            wewnetrzne[x + 7] = '-'
+                            wewnetrzne[x + 14] = 'M'
+                            bigVector.append(wewnetrzne.copy())
+                            del wewnetrzne[:]
+                            zaszly_zmiany = 1
